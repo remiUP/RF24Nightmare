1c5d44783af8961747a0b5e75a1a21745f69b086
Heitz Remi <remi.heitz@student-cs.fr>
2020-04-01 17:15:45 +0200

    Changed to finite state machine architecture

--- RX/RX.ino
+++ RX/RX.ino
@@ -1,41 +1,199 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <Adafruit_NeoPixel.h>
+
+#define CRqst 0
+#define AAdrs 1
+#define StpEN 2
+#define SrtEN 3
+#define Rst 4
 
 RF24 radio(9, 10); // CE, CSN
 byte self[6] = "0Node";
 byte child[6] = "0Node";
-int address = 1;
+byte gate[6] = "0Node";
+byte voidAddress[6] = "00000";
+int address = 0;
 
 
+int state = 0;
+int prevState = 0;
+unsigned long timer = 0;
+unsigned long ackTimeout = 10000;
+unsigned long sendNewCRqstDelay = 100;
+
+uint8_t sender = 0; 
 
 struct Payload {
-  int address;
+  unsigned int address;
+  unsigned int instruction;
   unsigned long data;
 };
 
 Payload res;
 
+
 void setup() {
-  self[0] += address;
-  child[0] += address+1;
-  
   Serial.begin(9600);
   radio.begin();
-  radio.openWritingPipe(child);
-  radio.openReadingPipe(1, self);   //Setting the address at which we will receive the data
-  radio.startListening();              //This sets the module as receiver
+  while(!Serial.available()){};
   }
-  void loop(){
-  if (radio.available())              //Looking for the data.
-  {
+  
+
+void loop(){
+  prevState = state;
+  switch (state) {
+    case 0:
+      Serial.println("State 0");
+      Serial.println("State 0 - Initializing");
+      radio.openWritingPipe(gate);
+      radio.stopListening();
+      state = 1;
+      break;
+      
+    case 1:
+      Serial.println("State 1");
+      if(millis()-timer > sendNewCRqstDelay){
+        timer = millis();
+        Payload P = {0,0,0};
+        Serial.println("Sending CRqst");
+        if(radio.write(&P,sizeof(P))){
+          Serial.println("Ack received");
+          radio.openReadingPipe(1,gate);
+          radio.startListening();
+          timer = millis();
+          state = 2;
+          Serial.println("Waiting for AAdrs");
+        }
+        else{
+          Serial.println("Ack not received");
+        }
+      }
+      break;
+
+    case 2:
+      Serial.println("State 2");
+      if (radio.available()){
+        Serial.println("Received payload");
+        if (readIncoming()){
+          Serial.println("Payload is addressed to this node");
+          if(res.address == 0 && res.instruction == AAdrs){
+            Serial.println("Payload is a AAdrs");
+            state = 3;
+            updateAddresses();
+          }
+        }
+      }
+      else if (millis()-timer > ackTimeout){
+        Serial.println("CRqst timed out");
+        state = 0;
+      }
+      break;
+
+    case 3:
+      Serial.println("State 3");
+      Serial.println("Setting up");
+      radio.openReadingPipe(1,self);
+      radio.openReadingPipe(2,gate);
+      radio.startListening();
+      state = 4;
+      break;
+    
+    case 4:
+      if (radio.available(&sender)){
+        Serial.println("Received Payload");
+        if (readIncoming()){
+          Serial.println("Payload is addressed to this node");
+          if (sender == 1 && res.instruction == StpEN){
+            Serial.println("Instructed to stop expanding the network by self pipe");
+            radio.openReadingPipe(2,voidAddress);
+            state = 6;
+          }
+          else if (sender == 2 && res.instruction == CRqst){
+            Serial.println("Payload is a CRqst from gate pipe");
+            radio.openWritingPipe(gate);
+            radio.stopListening();
+            state = 5;
+          }
+          else if (sender == 1 && res.instruction == Rst){
+            Serial.println("Instructed to reset by self pipe");
+            delay(res.data);
+            state = 0;
+          }
+        }
+      }
+      Serial.println("breaking case 4");
+      break;
+
+
+      
+    case 6:
+      if (radio.available()){
+        Serial.println("Received Payload");
+        if (readIncoming()){
+          Serial.println("Payload is addressed to this node");
+          if (res.instruction == SrtEN){
+            Serial.println("Instructed to start expanding network by self pipe");
+            state = 3;
+          }
+          if (res.instruction == Rst){
+            Serial.print("Instructed to reset by self pipe with delay : ");
+            Serial.println(res.data);
+            delay(res.data);
+            state = 0;
+          }
+        }
+      }
+      Serial.println("breaking case 6");
+      break;
+
+    case 5:
+      Serial.println("State 5");
+      Payload p = {0,AAdrs,address+1};
+      
+      if (radio.write(&p,sizeof(p))){
+        Serial.println("Address assignment successful");
+        radio.openWritingPipe(child);
+        radio.startListening();
+        state = 6;
+      }
+      else{
+        Serial.println("Address assignment failed");
+        state = 3;
+      }
+      Serial.println("breaking case 5");
+      break;
+    
+    default:
+      Serial.println("Default state");
+      break; 
+  }
+  if (prevState != state){
+    Serial.println("------");
+    Serial.println(state);
+    Serial.println("------");
+  }
+  delay(5);
+}
+
+
+
+void updateAddresses(){
+  address = int(res.data);
+  self[0] = address;
+  child[0] = address+1;
+}
+
+bool readIncoming(){
+  Serial.println("Reading Incoming");
   radio.read(&res, sizeof(res));    //Reading the data
-  if(res.address == address){
+  if(res.address == address or res.address == 0){
+    Serial.println(res.instruction);
     Serial.println(res.data);
     Serial.println("**************************");
+    return true;
   }
-  else{
+  else if(state == 6){
     radio.stopListening();
     if(radio.write(&res,sizeof(res))){
       Serial.println("Message forwarded");
@@ -44,7 +202,6 @@ void setup() {
       Serial.println("Transmission to child failed");
     }
     radio.startListening();
+    return false;
   }
-  }
-  delay(5);
-} 
+}
--- TX/TX.ino
+++ TX/TX.ino
@@ -1,56 +1,121 @@
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-RF24 radio(9, 10); // CE, CSN         
+#include <SimpleCLI.h>
 
-unsigned long data;
+#define CRqst 0
+#define AAdrs 1
+#define StpEN 2
+#define SrtEN 3
+#define Rst 4
+
+RF24 radio(9, 10); // CE, CSN
 byte self[6] = "0Node";
 byte child[6] = "0Node";
-int address = 0;
+byte gate[6] = "0Node";
+byte voidAddress[6] = "00000";
+int address = 1;
 
-int target = 1;
-
+int target = 2;
+unsigned long timer;
+unsigned long pairingTimeout = 10000;
 
 struct Payload {
-  int address;
+  unsigned int address;
+  unsigned int instruction;
   unsigned long data;
 };
 
 Payload res;
 
+SimpleCLI cli;
+Command cmdPair;
+Command cmdSend;
+
+
+
 void setup() {
-  self[0] += address;
-  child[0] += address+1;
+  self[0] = address;
+  child[0] = address + 1;
   
   Serial.begin(9600);
   radio.begin();                  //Starting the Wireless communication
-  radio.openWritingPipe(child); //Setting the address where we will send the data
-  radio.openReadingPipe(1,self);
-  radio.stopListening();          //This sets the module as transmitter
+  cmdPair = cli.addCommand("pair");
+  cmdSend = cli.addCommand("send");
+  cmdSend.addArgument("t");
+  cmdSend.addArgument("i",120);
+  cmdSend.addArgument("d",0);
 }
+
 void loop()
 {
   if(Serial.available()){
-  data = Serial.parseInt();
-  Serial.read();
-  Payload P = { target, data };
-  unsigned long start= millis();
-  if(radio.write(&P, sizeof(P))){                 //Sending the message to receiver
-  Serial.println("Message sent");
-  //radio.startListening();
-  //while(!radio.available()){}
-  //radio.read(&res,sizeof(res));
-  //unsigned long finish = millis();
-  //Serial.println(finish-start);
-  //radio.stopListening();
-  //Serial.println(P.data);
-  //Serial.println(P.adress);
-  //Serial.println("**************************");
-  
-  }
-  else{
-    Serial.println("Transmission failed");
-  }
+    String input = Serial.readStringUntil('\n');
+    Serial.println(">" + input);
+    cli.parse(input);
+    }
+  if (cli.available()){
+    Command cmd = cli.getCommand();
+    if (cmd == cmdPair){
+      pairNode();
+    }
+    if (cmd==cmdSend){
+      Argument argT = cmd.getArgument("t");
+      Argument argI = cmd.getArgument("i");
+      Argument argD = cmd.getArgument("d");
+      String argTval = argT.getValue();
+      unsigned int t = argTval.toInt();
+      String argIval = argI.getValue();
+      unsigned int i = argIval.toInt();
+      String argDval = argD.getValue();
+      unsigned long d = argDval.toInt();
+      Payload P = {t,i,d};
+      radio.stopListening();
+      radio.openWritingPipe(child);
+      if (radio.write(&P,sizeof(P))){
+        Serial.println("Transmition successful");
+      }
+      else{
+        Serial.println("Transmition failed");
+      }
+    }
   }
 delay(5);
+}
+
+void pairNode(){
+  Serial.println("Trying to pair new node");
+  radio.openReadingPipe(1,gate);
+  radio.startListening();
+  timer = millis();
+  while(!radio.available()) {
+    if (millis()-timer>pairingTimeout){
+      Serial.println("Pairing Timed-out");
+      return;
+    }
+    };
+  readIncoming();
+  if (res.instruction == CRqst){
+    Serial.println("Received CRqst");
+    radio.openWritingPipe(gate);
+    radio.stopListening();
+    Payload P = {0,AAdrs,address+1};
+    if (radio.write(&P,sizeof(P))){
+      Serial.println("Pairing successful");
+    }
+    else{
+      Serial.println("Node did not ack AAdrs");
+    }
+  }
+  else{
+    Serial.println("Received payload is not a CRqst");
+  }
+}
+
+
+void readIncoming(){
+  radio.read(&res, sizeof(res));    //Reading the data
+  Serial.println(res.instruction);
+  Serial.println(res.data);
+  Serial.println("**************************");
 }
